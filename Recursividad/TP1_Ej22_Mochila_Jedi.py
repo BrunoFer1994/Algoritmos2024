5a6ead4980e486abc23a32d06d520f6ed0b8db75
Bruno Fernandez <brunofernandez2008@gmail.com>
2024-04-15 20:42:51 -0300

    EjercicioParaEntregar

--- Recursividad/TP1_Ej22_Mochila_Jedi.py
+++ Recursividad/TP1_Ej22_Mochila_Jedi.py
@@ -5,7 +5,7 @@ ayuda de la fuerza” realizar las siguientes actividades:
 a. sacar los objetos de la mochila de a uno a la vez hasta encontrar un sable de luz o que no
 queden más objetos en la mochila;
 b. determinar si la mochila contiene un sable de luz y cuantos objetos fueron necesarios sacar para encontrarlo;
-c. Utilizar un vector para representar la mochila."""
+c. Utilizar un vector para representar la mochila. """
 
 
 mochila = ['Pan','Capa','Sable de luz','Otro']
